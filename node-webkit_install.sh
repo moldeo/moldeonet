ace124bb4279091c55a259e0a207bbbb33644b63
Moldeo Interactive <info@moldeointeractive.com.ar>
2015-07-01 12:00:01 -0300

    Added npm install script with node-osc, md5-node, osenv, moment

--- node-webkit_install.sh
+++ node-webkit_install.sh
@@ -1,6 +1,12 @@
 NWJS_PACKAGE_NAME=nwjs-v0.12.2-linux-x64
 NWJS_URL=http://dl.nwjs.io/v0.12.2/nwjs-v0.12.2-linux-x64.tar.gz
 
+
+sudo apt-get install npm
 wget $NWJS_URL
 tar -zxvf $NWJS_PACKAGE_NAME.tar.gz
 mv $NWJS_PACKAGE_NAME nwjs
+
+cd MoldeoControl
+npm install node-osc md5-node moment osenv
+
