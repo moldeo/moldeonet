907e124b9e062d307bba8f1cbaac2f62f48936ad
Moldeo Interactive <info@moldeointeractive.com.ar>
2015-07-01 16:59:01 -0300

    Scriptchecks for files and directories

--- node-webkit_install.sh
+++ node-webkit_install.sh
@@ -1,12 +1,28 @@
-NWJS_PACKAGE_NAME=nwjs-v0.12.2-linux-x64
-NWJS_URL=http://dl.nwjs.io/v0.12.2/nwjs-v0.12.2-linux-x64.tar.gz
+#!/bin/bash
+NWJS_VERSION=v0.12.2
+NWJS_PACKAGE_NAME=nwjs-$NWJS_VERSION-linux-x64
+NWJS_PACKAGE_FILE=$NWJS_PACKAGE_NAME.tar.gz
+NWJS_URL=http://dl.nwjs.io/$NWJS_VERSION/$NWJS_PACKAGE_FILE
 
+echo "Installing nwjs (node-webkit) version $NWJS_VERSION"
+
+FILE_LOCAL=./$NWJS_PACKAGE_FILE
+echo $FILE_LOCAL
+if [ -a $FILE_LOCAL ]; then
+    echo "$FILE_LOCAL ok!"
+    #    tar -zxvf NWJS_PACKAGE_FILE
+    #    mv $NWJS_PACKAGE_NAME nwjs        
+else        
+    echo "./$NWJS_PACKAGE_FILE not found, downloading:"
+    wget $NWJS_URL
+    tar -zxvf $NWJS_PACKAGE_FILE
+    mv $NWJS_PACKAGE_NAME nwjs        
+fi
 
 sudo apt-get install npm
-wget $NWJS_URL
-tar -zxvf $NWJS_PACKAGE_NAME.tar.gz
-mv $NWJS_PACKAGE_NAME nwjs
-
-cd MoldeoControl
-npm install node-osc md5-node moment osenv
-
+if [ -d MoldeoControl ]
+then
+    cd MoldeoControl
+    npm install node-osc md5-node moment osenv
+    cd ..
+fi
