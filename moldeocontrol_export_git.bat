ab8318ba82da1dfb6fd3101e719d759f18c09ddd
fabriciocosta <fabricio.costa@moldeointeractive.com.ar>
2015-07-04 15:04:24 -0300

    added export for git (simil export svn)

---
+++ moldeocontrol_export_git.bat
@@ -0,0 +1,11 @@
+git archive --format zip --output MoldeoControl.zip HEAD:MoldeoControl/
+mkdir z
+7z.exe x -y MoldeoControl.zip -o./z/MoldeoControl
+cp "package.moldeocontrol.json"  "./z/MoldeoControl/package.json"
+cp "config.init.js"  "./z/MoldeoControl/config.init.js"
+7z.exe a node_modules.zip ./MoldeoControl/node_modules
+7z.exe x -y node_modules.zip -o./z/MoldeoControl
+
+del MoldeoControl.zip
+7z.exe a MoldeoControl.zip ./z/MoldeoControl
+cp MoldeoControl.zip ../../packages/install/win
