2ec466b8f845838f6eb1e113168987d6afcecaf3
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2016-04-26 12:10:15 +0000

    Adding ids record to kill later

--- utils/start_facedetection.sh
+++ utils/start_facedetection.sh
@@ -1,4 +1,8 @@
 mkdir /tmp/dstblobs
 moldeoplayersdl2 -mol /home/pi/moldeoinstaller/moldeosamples/basic/09_OpenCV/09_OpenCV.mol &
+MOLDEO=$!
+echo $MOLDEO > moldeo.id
 LD_LIBRARY_PATH=/usr/local/lib mjpg_streamer  -i "input_file.so -f /tmp/dstblobs -n dstblobs.jpg" -o "output_http.so -w /usr/local/www" &
+MJPGSTREAMER=$!
+echo $MJPGSTREAMER > mjpgstreamer.id
 
