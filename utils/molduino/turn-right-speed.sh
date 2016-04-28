acf694f1b21b02b4f459697c8b1aa952bd16e925
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2016-04-28 10:26:58 +0000

    fixing all

--- utils/molduino/turn-right-speed.sh
+++ utils/molduino/turn-right-speed.sh
@@ -2,4 +2,4 @@
 SPEED2=$1
 SPEED1=$(expr 255 - $SPEED2 )
 echo "turn right at " $SPEED2
-i2cset -y 1 0x08 0x03 0x05 $SPEED1 i && i2cset -y 1 0x08 0x06 0x08 $SPEED2 i
+i2cset -y 1 0x08 0x05 $SPEED1 i && i2cset -y 1 0x08 0x08 $SPEED2 i
