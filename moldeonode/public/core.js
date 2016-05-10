f7682f72fc1f23aeb17b4821b439bf5428b77404
Moldeo Interactive <info@moldeointeractive.com.ar>
2016-05-10 08:06:40 -0300

    Added programs database

--- moldeonode/public/core.js
+++ moldeonode/public/core.js
@@ -3,7 +3,7 @@ var molduinoTasks = angular.module('molduinoTasks', []);
 
 function mainController($scope, $http) {
     $scope.formData = {};
-    $scope.formCode = {};
+    $scope.formProgram = {};
 
     // when landing on the page, get all todos and show them
     $http.get('/api/tasks')
@@ -15,9 +15,10 @@ function mainController($scope, $http) {
             console.log('Error: ' + data);
         });
 
-    $http.get('/api/codes')
+    $http.get('/api/programs')
         .success(function(data) {
-            $scope.codes = JSON.parse(data);
+            $scope.programs = data;
+            //JSON.parse(data);
             console.log(data);
         })
         .error(function(data) {
@@ -25,10 +26,10 @@ function mainController($scope, $http) {
         });
 
 
-    $scope.executeAction = function(command) {
+    $scope.executeAction = function(command,parameter) {
 
 
-      if (command=='code-play') {
+      if (command=='program-play') {
         /** send code, compile and run it*/
 /**
         var code = Blockly.JavaScript.workspaceToCode(workspace);
@@ -37,33 +38,59 @@ function mainController($scope, $http) {
           editor.getSession().setValue(code);
         }
 */
-        $scope.formCode.text = editor.getSession().getValue();
-        console.log("code to be compiled and run in server:", $scope.formCode.text);
+        //$scope.formCode.text =
+        //alert("Playing: " + editor.getSession().getValue() );
+        //$scope.formCode.text
+        $scope.code = editor.getSession().getValue();
+        console.log( "code to be compiled and run in server:",  $scope.code );
 
-        $http.post('/api/code', { text: $scope.formCode.text } )
+        $http.post('/api/program/run', { text:  $scope.code } )
                 .success(function(data) {
-                    $scope.runresult = data;
+                    //$scope.runresult = data;
                     console.log(data);
-
-
                 })
                 .error(function(data) {
                     console.log('Error: ' + data);
                 });
-      } else
-      if (command=='code-debug') {
+
+      } else if (command=='program-load') {
+                //$("#selprogram").attr("idp", data._id );
+                //$("#selprogram").html( data.name );
+                console.log( "exec program-load", parameter );
+                editor.getSession().setValue( parameter );
 
       } else
-      if (command=='code-abort') {
+      if (command=='program-save') {
+
+        var mobj = {
+                    id: $("#selprogram").attr("idp"),
+                    text: editor.getSession().getValue()
+                  };
+
+        console.log("program-save", mobj );
+
+
+        $http.post('/api/programs/save', mobj )
+                .success(function(data) {
+                    //$scope.runresult = data;
+                    console.log("success save:", data);
+                })
+                .error(function(data) {
+                    console.log('Error saving: ' + data);
+                });
+
+      } else
+      if (command=='program-debug') {
+
+      } else
+      if (command=='program-abort') {
         /** send code, compile and run it*/
         console.log("stop and abort any code running right now in server");
 
-        $http.post('/api/code', { text: "Molduino.Loop = false;" } )
+        $http.post('/api/program/run', { text: "Molduino.Loop = false;" } )
                 .success(function(data) {
                     $scope.runresult = data;
                     console.log(data);
-
-
                 })
                 .error(function(data) {
                     console.log('Error: ' + data);
@@ -75,8 +102,6 @@ function mainController($scope, $http) {
                     $scope.formData = {}; // clear the form so our user is ready to enter another
                     $scope.tasks = data;
                     console.log(data);
-
-
                 })
                 .error(function(data) {
                     console.log('Error: ' + data);
@@ -90,6 +115,49 @@ function mainController($scope, $http) {
 
     $scope.refreshprogram = function() {
         editor.getSession().setValue( document.getElementById("lastcode").value );
+    };
+
+    // when submitting the add form, send the text to the node API
+    $scope.createProgram = function() {
+        console.log("scope.formProgram:",$scope.formProgram);
+        $http.post('/api/programs', $scope.formProgram )
+            .success(function(data) {
+                //$scope.formProgram = {}; // clear the form so our user is ready to enter another
+                $scope.programs = data;
+                console.log("success! data:", data, $scope.formProgram);
+                $("#selprogram").html($scope.formProgram.name);
+                $("#selprogram").attr('idp', data[data.length-1]._id );
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    $scope.loadProgram = function(program) {
+        $http.post('/api/programs/load', program )
+            .success(function(data) {
+                //$scope.formProgram = {}; // clear the form so our user is ready to enter another
+                //$scope.programs = data;
+                console.log("load program: data:", data);
+                $("#selprogram").attr("idp", data._id );
+                $("#selprogram").html( data.name );
+                $scope.executeAction('program-load',data.text)
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a todo after checking it
+    $scope.deleteProgram = function(id) {
+        $http.delete('/api/programs/' + id)
+            .success(function(data) {
+                $scope.programs = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
     };
 
     // when submitting the add form, send the text to the node API
