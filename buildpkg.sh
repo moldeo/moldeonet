2bbb9c5f929d07327753cfb97b0f55245d67481d
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2018-05-01 13:50:18 -0300

    Version 1.1.0.0

--- buildpkg.sh
+++ buildpkg.sh
@@ -68,14 +68,52 @@ echo "extracting..."
 
 cd deb/moldeonet-*
 dh_make -i -e info@moldeo.org -p moldeonet -c gpl3
-gedit ../../control.amd64.11.10 debian/control ../../moldeonet.dirs debian/moldeonet.dirs ../../moldeonet.install debian/moldeonet.install ../../moldeonet-dev.dirs debian/moldeonet-dev.dirs ../../moldeonet-dev.install debian/moldeonet-dev.install debian/changelog
 
-echo " 
-Now execute: 
- cd deb/moldeonet-*
- dpkg-buildpackage -us -uc -rfakeroot 2>&1 | tee ../../buildpkg_logs.txt
-"
+echo "Success: Extraction"
+
+sed -i -e 's/Architecture: any/Architecture: amd64/g' debian/control
+sed -i -e 's/Section: unknown/Section: graphics/g' debian/control
+sed -i -e 's/Maintainer: fabricio /Maintainer: Moldeo Interactive /g' debian/control
+sed -i -e 's/<insert the upstream URL, if relevant>/http:\\\\www.moldeo.org/g' debian/control
+#sed -i -e 's/moldeonetBROKEN/moldeoplugins/g' debian/control
+#sed -i -e 's/,autotools-dev/, autotools-dev, libmoldeo-dev/g' debian/control
+
+#development
+#sed -i -e '0,/, ${misc:Depends}/{s/, ${misc:Depends}/,${misc:Depends}/g}' debian/control
+#sed -i -e '0,/<insert up to 60 chars description>/{s/<insert up to 60 chars description>/Moldeo Plugins, Dev Package/}'  debian/control
+
+#binary
+sed -i -e 's/${misc:Depends}/${misc:Depends}, moldeoplayer, libmoldeo, moldeoplugins, moldeosamples/g' debian/control
+sed -i -e 's/<insert up to 60 chars description>/Moldeo 1.0/'  debian/control
+sed -i -e 's/<insert long description, indented with spaces>/Moldeo 1.0 is a set of interface controls, bash scripts and other tools for Moldeo Platform. MoldeoControl, MoldeoJS, Molduino/g'  debian/control
+
+sed -i -e 's/unstable/experimental/g' debian/changelog
+sed -i -e 's/fabricio/Moldeo Interactive/g' debian/changelog
+sed -i -e 's/Initial release (Closes: #nnnn)  <nnnn is the bug number of your ITP>/Initial release/g' debian/changelog
+#sed -i -e 's///g'  debian/control
+#
+#sed -i -e 's///g' debian/control
+
+echo "usr/bin" > debian/moldeonet.dirs
+echo "usr/bin/nwjs" >> debian/moldeonet.dirs
+echo "usr/bin/nwjs/locales" >> debian/moldeonet.dirs
+echo "usr/share/applications" >> debian/moldeonet.dirs
+echo "usr/share/moldeonet" >> debian/moldeonet.dirs
 
 
-echo "Success: extraction"
+echo "debian/moldeonet/usr/bin/*" > debian/moldeonet.install
+echo "debian/moldeonet/usr/bin/nwjs/*" >> debian/moldeonet.install
+echo "debian/moldeonet/usr/bin/nwjs/locales/*" >> debian/moldeonet.install
+echo "debian/moldeonet/usr/share/applications/*" >> debian/moldeonet.install
+echo "debian/moldeonet/usr/share/moldeonet/*" >> debian/moldeonet.install
+
+#echo "usr/lib" > debian/moldeonet-dev.dirs
+
+#echo "usr/lib/moldeo/preeffects/lib*.a" > debian/moldeonet-dev.install
+
+xed ../../control.amd64.11.10 debian/control ../../moldeonet.dirs debian/moldeonet.dirs ../../moldeonet.install debian/moldeonet.install ../../moldeonet-dev.dirs debian/moldeonet-dev.dirs ../../moldeonet-dev.install debian/moldeonet-dev.install debian/changelog
+
+dpkg-buildpackage -us -uc -rfakeroot 2>&1 | tee ../../buildpkg_logs.txt
+
+echo "Success: package generated"
 exit 0
